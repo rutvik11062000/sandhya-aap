f53c2d75ad810d5bf9ad9cdc453c3c57e78f9f32
Rutvik Chandla <rutvik.chandla@gmail.com>
2022-10-30 16:04:00 +0530

    added db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.create!(name: "Regular")
+Product.create!(name: "PreInk")
+Product.create!(name: "Printing")
+Product.create!(name: "Date")
